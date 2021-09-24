a087ac0c4a482d6f217cec97308011b0cb395f18
Daniel Henriques <danielhrqs4@yahoo.com>
2021-09-23 21:09:31 -0300

    firstCommit

---
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula01OlaDevDojo {
+    //Comentário em linha
+    /*
+        Comentário
+        de
+        Múltiplas
+        linhas
+    */
+
+    /**
+     *
+     * Comentário em linha <b>javaDoc</b>
+     */
+
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
---
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        /*
+          Os tipos primitivos são:
+          int, double, float, char, byte, shirt, long, boolean
+        */
+        int age = (int) 100000000000L; //Corta os bits para caber na força, valor imprevisível
+        double salaryDouble = 2000;
+        float salaryFloat = 2500.0f;//Para especificar que é um float ou d para especificar um double
+        char caractere = 'c';
+        byte ageByte = 10;
+        short ageShort = 10;
+        long bigNumber = 1000000000;
+        boolean True = true;
+        boolean False = false;
+        System.out.println("A idade é " + age + " anos");
+        System.out.println(True);
+    }
+}
